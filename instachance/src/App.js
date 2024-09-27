c1e82174f91e32294b19bc81fd132f8ecbf445d3
Chaunceyo <chaunceyogletree725@gmail.com>
2024-09-27 18:33:03 -0400

    wip

---
+++ instachance/src/App.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import SearchBar from './frontend/components/SearchBar/SearchBar';
+import NavBar from './frontend/components/NavBar/NavBar';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <div className='insta-header'>
+          <h1>instaChance</h1>
+        </div>
+        <SearchBar/>
+        <NavBar/>
+        <nav className='App-nav'>
+          <Routes>
+            <Route></Route>
+          </Routes>
+        </nav>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ instachance/src/frontend/components/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SearchBar = () => {
+    return (
+       <div className="search-bar-wrapper">
+            <br/>
+            <form className="search-form">
+                <input type="text" placeholder="What's on your mind?"/>
+            </form>
+            <br/>
+        </div> 
+
+    )
+}
+
+export default SearchBar
